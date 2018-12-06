edfa649b020959d86f4b8a6901c9ec04b4ebe8a0
ALi <s.arme@ya.ru>
2018-12-06 15:02:27 +0330

    Initial Commit

---
+++ PasswordManager/Source.cpp
@@ -0,0 +1,169 @@
+#include <nana/gui.hpp>
+#include <nana/gui/widgets/label.hpp>
+#include <nana/gui/widgets/button.hpp>
+#include <nana/gui/widgets/listbox.hpp>
+#include <nana/gui/widgets/textbox.hpp>
+#include <string>
+
+int add(int, int);
+
+using namespace nana;
+using namespace nana;
+
+//Creates a textbox and button
+//textbox shows the value of the sub item
+//button is used to delete the item.
+class inline_widget : public listbox::inline_notifier_interface {
+private:
+	//Creates inline widget
+	//listbox calls this method to create the widget
+	//The position and size of widget can be ignored in this process
+	virtual void create(window wd) override {
+		//Create listbox
+		txt_.create(wd);
+		txt_.events().click([this] {
+			//Select the item when clicks the textbox
+			indicator_->selected(pos_);
+		});
+
+		txt_.events().mouse_move([this] {
+			//Highlight the item when hovers the textbox
+			indicator_->hovered(pos_);
+		});
+
+		txt_.events().key_char([this](const arg_keyboard& arg) {
+			if (arg.key == keyboard::enter) {
+				//Modify the item when enter is pressed
+				arg.ignore = true;
+				indicator_->modify(pos_, txt_.caption());
+			}
+		});
+		//Or modify the item when typing
+		txt_.events().text_changed([this]() {
+			indicator_->modify(pos_, txt_.caption());
+		});
+		//Create button
+		btn_.create(wd);
+		btn_.caption("Delete");
+		btn_.events().click([this] {
+			//Delete the item when button is clicked
+			auto & lsbox = dynamic_cast<listbox&>(indicator_->host());
+			lsbox.erase(lsbox.at(pos_));
+		});
+
+		btn_.events().mouse_move([this] {
+			//Highlight the item when hovers the button
+			indicator_->hovered(pos_);
+		});
+	}
+
+	//Activates the inline widget, bound to a certain item of the listbox
+	//The inline_indicator is an object to operate the listbox item,
+	//pos is an index object refers to the item of listbox
+	virtual void activate(inline_indicator& ind, index_type pos) {
+		indicator_ = &ind;
+		pos_ = pos;
+	}
+
+	void notify_status(status_type status, bool status_on) override {
+		//Sets focus for the textbox when the item is selected
+		if ((status_type::selecting == status) && status_on)
+			txt_.focus();
+	}
+
+	//Sets the inline widget size
+	//dimension represents the max size can be set
+	//The coordinate of inline widget is a logical coordinate to the sub item of listbox
+	void resize(const size& dimension) override {
+		auto sz = dimension;
+		sz.width -= (sz.width < 50 ? 0 : 50); //Check for avoiding underflow.
+		txt_.size(sz);
+
+		rectangle r(sz.width + 5, 0, 45, sz.height);
+		btn_.move(r);
+	}
+
+	//Sets the value of inline widget with the value of the sub item
+	virtual void set(const value_type& value) {
+		//Avoid emitting text_changed to set caption again, otherwise it
+		//causes infinite recursion.
+		if (txt_.caption() != value)
+			txt_.caption(value);
+	}
+
+	//Determines whether to draw the value of sub item
+	//e.g, when the inline widgets covers the whole background of the sub item,
+	//it should return false to avoid listbox useless drawing
+	bool whether_to_draw() const override {
+		return false;
+	}
+private:
+	inline_indicator * indicator_{ nullptr };
+	index_type pos_;
+	textbox txt_;
+	button btn_;
+};
+
+int main() {
+
+	////Define a form.
+	form fm;
+
+	////Define a label and display a text.
+	//label lab{ fm, "Hello, <bold blue size=16>Nana C++ Library</>" };
+	//lab.format(true);
+
+	//Define a button and answer the click event.
+	button qut{ fm, "Quit" };
+	qut.events().click([&fm] {
+		fm.close();
+	});
+	
+	button add{ fm, "Add" };
+	add.events().click([&fm] {
+		fm.close();
+	});
+
+
+	////Show the form
+	//fm.show();
+
+	////Start to event loop process, it blocks until the form is closed.
+	//exec();
+	//lab.caption(std::string("Ali"));
+	//fm.show();
+	//form fm;
+	listbox lsbox(fm, rectangle{ 10, 10, 300, 200 });
+
+	//Create two columns
+	lsbox.append_header("column 0");
+	lsbox.append_header("column 1");
+
+	//Then append items
+	lsbox.at(0).append({ "Hello0", "World0" });
+	lsbox.at(0).append({ "Hello1", "World1" });
+	lsbox.at(0).append({ "Hello2", "World2" });
+	lsbox.at(0).append({ "Hello3", "World3" });
+
+
+	//Set the inline_widget, the first column of category 0, the second column of category 1
+	lsbox.at(0).inline_factory(0, pat::make_factory<inline_widget>());
+	//Layout management
+	//fm.div("vert <><<><weight=80% text><>><><weight=24<><button><>><>");
+	fm.div("< <vert <weight=80%<weight=80% text>| <vert <><add><> >> |<button>> > ");
+	fm["text"] << lsbox;
+	fm["button"] << qut;
+	fm["add"] << add;
+	fm.collocate();
+
+	fm.show();
+	exec();
+}
+
+int add(int, int) {
+	__asm
+	{
+		mov   eax, [esp + 4]; argument 1
+		add   eax, [esp + 8]; argument 2
+	}
+}
